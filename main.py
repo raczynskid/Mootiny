1b37671589f8f26b155bf4eeef921b91f62bc363
Dawid <raczynski90@gmail.com>
2020-04-17 23:50:22 +0200

    added selectable objects

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from game_libs.abstract_objects import Selection
+from game_libs.abstract_objects import Selection, Entity
 import pygame
 import sys
+from random import randint
 
 pygame.init()
 pygame.mixer.init()
@@ -16,8 +17,24 @@ selection = None
 draw_selection = False
 drawn_selections = []
 
+# Entities:
+entities = [Entity(screen, randint(100, 700), randint(100, 500), 10, "random") for e in range(500)]
+
+
+def draw_entities(entities_iterable):
+    """
+    loop through created entities and draw them on the surface
+    """
+    for entity in entities_iterable:
+        entity.draw()
+        if draw_selection:
+            entity.in_selection(selection)
+
 
 def draw_cursor(surface, position):
+    """
+    draw cursor on selected surface based on passed position
+    """
     pygame.draw.rect(surface, (255, 255, 255), (position[0], position[1], 5, 5), 5)
 
 
@@ -39,13 +56,13 @@ while True:
             selection = None
 
     screen.fill((0, 0, 0))
-    draw_cursor(screen, mousePosition)
+
+    draw_entities(entities)
+
     if draw_selection:
         selection.update(mousePosition[0], mousePosition[1])
         selection.draw(screen)
 
-    if drawn_selections:
-        for s in drawn_selections:
-            s.draw(screen)
+    draw_cursor(screen, mousePosition)
 
     pygame.display.update()
