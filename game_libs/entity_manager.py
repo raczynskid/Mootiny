4a39e2b992a18d0a865786482ed3475d3b4cbe3c
Dawid <raczynski90@gmail.com>
2020-05-02 16:52:35 +0200

    first commit of test file

---
+++ game_libs/entity_manager.py
@@ -0,0 +1,4 @@
+class EntityManager:
+    """class to handle creating and destroying entities in game
+    created entites should be added to EntityManager, and all events should be handled through it"""
+    pass
