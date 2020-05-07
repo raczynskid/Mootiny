0cd02b28c17832d1c4c85dff07b12f2c7d81ae5b
Dawid <raczynski90@gmail.com>
2020-05-08 00:45:39 +0200

    building management and create random cow

--- game_libs/entity_manager.py
+++ game_libs/entity_manager.py
@@ -1,3 +1,8 @@
+from random import randint
+
+from game_libs.game_objects import *
+
+
 class EntityManager:
     """class to handle creating and destroying entities in game
     created entites should be added to EntityManager, and all events should be handled through it"""
@@ -5,6 +10,7 @@ class EntityManager:
     def __init__(self):
         """initialize empty list to store existing entities"""
         self.entities = []
+        self.buildings = []
 
     def create_entity(self, entity):
         """add a new entity to the manager"""
@@ -14,14 +20,17 @@ class EntityManager:
         """remove an entity from the manager"""
         self.entities.remove(entity)
 
-    def draw_entities(self):
-        """
-        call draw method for all entites in manager
-        if entity does not have callable "draw" method, raise AttributeError but proceed
-        """
-        for e in self.entities:
-            try:
-                yield e.draw()
-            except AttributeError:
-                print("Object cannot be drawn")
-                pass
+    def create_building(self, building):
+        """add a new building to the manager"""
+        self.buildings.append(building)
+
+    def destroy_building(self, building):
+        """remove an building from the manager"""
+        self.buildings.remove(building)
+
+    def create_random_cow(self):
+        """place a random cow somewhere on the map"""
+        cow = Cow((randint(10, Constants.WINDOW_WIDTH - 10), randint(10, Constants.WINDOW_HEIGHT - 200)))
+        cow.set_speed(10)
+        cow.set_bounce(False)
+        return cow
