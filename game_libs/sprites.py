5bf957aa157fc072e155b4471ca6d684f942f512
Dawid <raczynski90@gmail.com>
2020-05-08 00:43:16 +0200

    paths aligned with pyinstaller, Sprite class added color_key attribute

--- game_libs/sprites.py
+++ game_libs/sprites.py
@@ -1,9 +1,16 @@
 import pygame
-from game_libs.abstract_objects import Entity
-from game_libs import game_objects
+import sys
+import os
 
-spr_index = {'cow': r'./assets/sprites/cow_static.png',
-             'barn': r'./assets/sprites/barn.png'}
+
+def resource_path(relative_path):
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return base_path + relative_path
 
 
 def load_image(name):
@@ -19,8 +26,16 @@ def load_image(name):
 class Sprite:
     """sprite object with preloaded image to pass to game object constructor"""
 
-    def __init__(self, sprite_path):
+    def __init__(self, sprite_path, color_key=(0, 0, 0)):
         self.image, self.rect = load_image(sprite_path)
-        self.image.set_colorkey((0, 0, 0))
+        self.image.set_colorkey(color_key)
         self.surface = pygame.display.get_surface()
         self.area = self.surface.get_rect()
+
+    def change_color_key(self, rgb):
+        self.image.set_colorkey(rgb)
+
+
+spr_index = {'cow': resource_path(r'\assets\sprites\cow_static.png'),
+             'barn': resource_path(r'\assets\sprites\barn.png'),
+             'interface': resource_path(r'\assets\sprites\interface.png')}
