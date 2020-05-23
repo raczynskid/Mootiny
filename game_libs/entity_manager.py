d21fe8d6651e8cd1ac9ff4716e7d3c6db5cfd1e6
Dawid <raczynski90@gmail.com>
2020-05-24 00:16:13 +0200

    docstrings

--- game_libs/entity_manager.py
+++ game_libs/entity_manager.py
@@ -56,22 +56,28 @@ class EntityManager:
             spr.draw()
 
     def set_group_target(self, mg, target):
+        """initiated by click event, set targets for all selected entities"""
         for e in [e for e in self.entities if e.get_selection()]:
             if target in mg.closed_list:
                 print('abort')
                 return
+            # create path of nodes coordinates (row, column) with a* alogorythm
             path = (mg.a_star(mg.get_row_column_by_pixel_coords(e.get_position()), target))
             # todo: type error here sometimes, not sure why
+            # translate nodes coordinated into pixel coordinated
             pixel_path = [mg.get_pixel_coords_by_row_column(node) for node in path]
+            # set the path for entity
             e.set_path(pixel_path)
 
     def move_and_hover(self, mouse_pos):
+        """every frame check for action and apply movement and check for mouse hover to draw selection indicator"""
         for e in self.entities:
             # todo: need to change below line - the new node is set as soon as the top corner touches the node, wait until sprite fully inside to update node
             e.move()
             e.hover(mouse_pos)
 
     def check_cow_collisions(self):
+        """cow sprite collisions - probably to be deprecated in future"""
         cowlist = [cow for cow in self.entities if isinstance(cow, Cow)]
         for cow in cowlist:
             ix = cowlist.index(cow)
